081c0c460c69516f047bef8e6d6bbe0dffdce98b
Benjamin J Anderson <banderson@mashable.com>
2016-09-25 18:32:47 -0500

    begin fetch characters message

--- src/app/channels/userControl.js
+++ src/app/channels/userControl.js
@@ -3,10 +3,8 @@ import io from 'socket.io-client';
 export default (server, session) => {
   const socket = session.socket;
 
-  socket.on('get_characters', (data) => {
-    let user = data.user;
-    let characters = [];
-    server.logger.info('Getting characters for user: ');
+  socket.on('fetch_characters', (currentUser) => {
+    server.logger.info('Getting characters for user: ', currentUser);
   });
 
 };
