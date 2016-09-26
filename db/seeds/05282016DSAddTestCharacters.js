c707761a31d865763c71211ccc384861fed164d4
Benjamin J Anderson <banderson@mashable.com>
2016-09-25 20:50:47 -0500

    Fetch user characters

--- db/seeds/05282016DSAddTestCharacters.js
+++ db/seeds/05282016DSAddTestCharacters.js
@@ -1,22 +1,45 @@
+// classType: integer, see classes table
+// attunement: integer, 1 <Earth>, 2 <Fire>, 3 <Water>, 4 <Wind>
+// resistance <index = attunement>, <value = resistance value>
+//    index 0 = + % resistance modifier 
+// each attunement gains 10 resistance to their own element
+//
+//  Strength: Physical Attack
+//  Defense: Physical Defense
+//  Agility: Attack Speed / Order
+//  Vitality: HP
+//  Intelligence: Magical Attack
+//  Resistance: Magical Defense
+//
 module.exports = function(db, conn, conf) {
   return db.db(conf.database.name).table("characters").insert([
     {
       user: 'username',
-      name: 'Player-1',
-      specialty: 'Unknown',
-      strength: 1,
-      agility: 1,
-      vitality: 1,
-      dexterity: 1
+      name: 'Squire',
+      level: 1,
+      experience: 0,
+      classType: 1,
+      attunement: 1,
+      strength: 8,
+      defense: 5,
+      agility: 4,
+      vitality: 5,
+      intelligence: 3,
+      resistance: [0, 10, 0, 0, 0]
     },
     {
       user: 'username',
-      name: 'Player-2',
-      specialty: 'Unknown',
+      name: 'Acolyte',
+      level: 1,
+      experience: 0,
+      classType: 2,
+      attunement: 2,
       strength: 2,
-      agility: 2,
-      vitality: 2,
-      dexterity: 2
+      defense: 3,
+      agility: 5,
+      vitality: 5,
+      intelligence: 10,
+      resistance: [10, 0, 10, 0, 0]
     },
     ]).run(conn);
 };
--- src/app/channels/userControl.js
+++ src/app/channels/userControl.js
@@ -5,6 +5,25 @@ export default (server, session) => {
 
   socket.on('fetch_characters', (currentUser) => {
     server.logger.info('Getting characters for user: ', currentUser);
+    server.r.table('characters')
+    .filter(server.r.row('user')
+    .eq(currentUser.username)).run(server.conn, (err, cursor) => {
+      if (err) {
+        server.logger.info('Unable to retrieve a list of characters for user', currentUser.username);
+        socket.emit('fetch_characters', []);
+        return false;
+      }
+      return cursor.toArray((err, records) => {
+        if (err) {
+          server.logger.info('Unable to cast result set to array');
+          socket.emit('fetch_characters', []);
+          return false;
+        }
+        server.logger.info('Dispatching records', records);
+        socket.emit('fetch_characters', records);
+        return records;
+      });
+    });
   });
 
 };
